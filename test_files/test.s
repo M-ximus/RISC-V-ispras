ddddb0a37df6ac43049e25e71bec520cfe8675fd
Maximus <Davydov.MA@phystech.edu>
2019-08-09 09:36:00 +0300

    No_plt test on clang 10.0.0

--- test_files/test.s
+++ test_files/test.s
@@ -7,7 +7,11 @@ g:                                      # @g
 # %bb.0:                                # %entry
 	addi	sp, sp, -16
 	sd	ra, 8(sp)
-	call	f@plt
+.LBB0_1:                                # %entry
+                                        # Label of block must be emitted
+	auipc	a0, %got_pcrel_hi(f)
+	ld	a0, %pcrel_lo(.LBB0_1)(a0)
+	jalr	a0
 	negw	a0, a0
 	ld	ra, 8(sp)
 	addi	sp, sp, 16
