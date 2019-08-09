806f7621824006b832fa2ce489abb2b8b7dba424
Maximus <Davydov.MA@phystech.edu>
2019-08-09 08:48:30 +0300

    Clang 10.0.0 + hard-float

--- test_files/test.s
+++ test_files/test.s
@@ -7,11 +7,7 @@ g:                                      # @g
 # %bb.0:                                # %entry
 	addi	sp, sp, -16
 	sd	ra, 8(sp)
-.LBB0_1:                                # %entry
-                                        # Label of block must be emitted
-	auipc	a0, %got_pcrel_hi(f)
-	ld	a0, %pcrel_lo(.LBB0_1)(a0)
-	jalr	a0
+	call	f@plt
 	negw	a0, a0
 	ld	ra, 8(sp)
 	addi	sp, sp, 16
@@ -20,6 +16,6 @@ g:                                      # @g
 	.size	g, .Lfunc_end0-g
                                         # -- End function
 
-	.ident	"clang version 9.0.0 (https://github.com/llvm/llvm-project.git cb238de456ba2db699d969858eddbc0971c8592c)"
+	.ident	"clang version 10.0.0 (https://github.com/llvm/llvm-project.git b865d5a425ec17403eb77ea8175863a310ad6668)"
 	.section	".note.GNU-stack","",@progbits
 	.addrsig
